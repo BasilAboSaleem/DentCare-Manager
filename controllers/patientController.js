897f198ae84ce54450ff9ea63f3831688bba24bc
Basil Abo Saleem <basilsaleem11@gmail.com>
2025-07-22 14:22:18 +0300

    feat: add POST route for creating new patient and test successfully

--- controllers/patientController.js
+++ controllers/patientController.js
@@ -24,3 +24,27 @@ exports.add_patient_get = (req, res) => {
         title: "Add Patient"
     });
 }
+
+exports.add_patient_post = async (req, res) => {
+  try{
+    const { name, phone, address, dateOfBirth, gender, notes } = req.body;
+    const newPatient = new Patient({
+      name,
+      phone,
+      address,
+      dateOfBirth,
+      gender,
+      notes
+    });
+    await newPatient.save();
+    req.flash("success", "Patient added successfully");
+    res.redirect("/Patients");
+  }
+  catch (err) {
+    console.error("Error adding patient:", err);
+    res.status(500).render('pages/error/error-500', {
+      title: 'Server Error',
+      message: 'Something went wrong. Please try again later.'
+    });
+  }
+}
--- routes/patientRoute.js
+++ routes/patientRoute.js
@@ -6,5 +6,6 @@ const { requireAuth, checkIfUser } = require('../middlewares/authMiddlewares');
 
 router.get("/Patients", requireAuth, patientController.all_patients_get );
 router.get('/Patients/add', requireAuth, patientController.add_patient_get);
+router.post('/Patients/add', requireAuth, patientController.add_patient_post);
 
 module.exports = router;
