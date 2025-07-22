64948ebfe32ced401566f93279660cdc7cb76fd3
Basil Abo Saleem <basilsaleem11@gmail.com>
2025-07-22 15:44:00 +0300

    refactor: update Visit model to include reference to related Appointment

--- models/Visit.js
+++ models/Visit.js
@@ -3,6 +3,8 @@ const mongoose = require('mongoose');
 const visitSchema = new mongoose.Schema({
   patient: { type: mongoose.Schema.Types.ObjectId, ref: 'Patient', required: true },
   doctor: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  appointment: { type: mongoose.Schema.Types.ObjectId, ref: 'Appointment' },
+
   visitDate: { type: Date, default: Date.now },
   diagnosis: { type: String, trim: true },
   treatment: { type: String, trim: true },
