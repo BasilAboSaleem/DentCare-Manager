52a5910c62f226af13d3375a8b38969fac313023
Basil Abo Saleem <basilsaleem11@gmail.com>
2025-07-23 09:47:30 +0300

    feat(dashboard): add dynamic Today's Appointments section to dashboard

--- controllers/coreController.js
+++ controllers/coreController.js
@@ -1,5 +1,33 @@
+const User = require('../models/User');
+const Appointment = require('../models/Appointment');
+const Patient = require('../models/Patient');
 
 
-exports.index_get = (req, res) => {
-  res.render('index', { title: 'DentCare Manager' });
+exports.index_get = async (req, res) => {
+  try{
+    const startOfDay = new Date();
+    startOfDay.setHours(0,0,0,0);
+
+    const endOfDay = new Date();
+    endOfDay.setHours(23,59,59,999);
+
+    const appointments = await Appointment.find({
+      appointmentDate: { $gte: startOfDay, $lte: endOfDay }
+    })
+    .populate('patient', 'name')
+    .populate('doctor', 'name');
+
+    res.render('index', {
+      title: 'Dashboard',
+      appointments
+    });
+
+  
+  }catch (error) {
+    console.error('Error fetching index page:', error);
+    res.status(500).render('pages/error/error-500', {
+      title: 'Internal Server Error'
+    });
+  }
 };
+ 
